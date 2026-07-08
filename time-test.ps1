21ee0b648e86e4362883b65715f3f28038a4ee6b
Matthew <mattmadecley@gmail.com>
2026-07-08 10:50:22 -0400

    Remove local QA scratch files from repo

--- time-test.ps1
+++
@@ -1,5 +0,0 @@
-$sw = [Diagnostics.Stopwatch]::StartNew()
-$r = curl.exe -s --max-time 90 -H 'Authorization: Bearer Cleymadematt8@' 'https://street-beryl.vercel.app/api/cron/catalog?mode=classify&limit=3'
-$sw.Stop()
-Write-Output "ELAPSED: $($sw.Elapsed.TotalSeconds)s"
-Write-Output $r
