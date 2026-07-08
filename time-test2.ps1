d0e8dacc34a4aed0cfb45d70ffb83bff6d14aa08
Matthew <mattmadecley@gmail.com>
2026-07-08 11:01:20 -0400

    Remove stray test script from repo

--- time-test2.ps1
+++
@@ -1,5 +0,0 @@
-$sw = [Diagnostics.Stopwatch]::StartNew()
-$r = curl.exe -s --max-time 90 -H 'Authorization: Bearer Cleymadematt8@' 'https://street-beryl.vercel.app/api/cron/catalog?mode=classify-preview&limit=2'
-$sw.Stop()
-Write-Output "ELAPSED: $($sw.Elapsed.TotalSeconds)s"
-Write-Output $r
